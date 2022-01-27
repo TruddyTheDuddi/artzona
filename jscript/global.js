a0945d284cc42d7b7247fa6b80a5a5c65fcb4296
TruddyTheDuddi <ddemkoo.nolkooo@gmail.com>
2022-01-27 12:40:08 +0100

    Add slap animation

--- jscript/global.js
+++ jscript/global.js
@@ -20,5 +20,66 @@ function textareaSetup(){
     }
 }
 
+/**
+ * Starts slap animation
+ */
+function slapAnimate(b){
+    // Setup slap_fx
+    let slapFxElem = document.createElement("img");
+    slapFxElem.classList.add("slap_paw");
+    slapFxElem.src = "img/site/slap.svg";
+    slapFxElem.id = "slap_paw";
+
+    // Add
+    b.insertAdjacentHTML('beforeend', slapFxElem.outerHTML);
+
+    // Get element
+    let nodes = b.querySelectorAll("#slap_paw");
+    let addedEl = nodes[nodes.length-1];
+    let pos = ((Math.random() - 0.5) * 100);
+    addedEl.style.left = (pos + 20) + "px";
+
+    // Timeout for text spawn
+    setTimeout(function(){
+        slapAnimateText(b, pos);
+    }, 200)
+
+    // Timeout for post block move
+    setTimeout(function(){
+        b.parentElement.parentElement.style.animation = "none";
+        b.offsetHeight;
+        b.parentElement.parentElement.style.animation = "shake 1s cubic-bezier(0.19, 1, 0.22, 1)";
+    }, 300)
+
+    // Timeout for removal
+    setTimeout(function(){
+        b.removeChild(addedEl);
+    }, 600)
+}
+
+/**
+ * Create slap text
+ */
+function slapAnimateText(b, pos){
+    // Setup slap_fx
+    let slapFxElem = document.createElement("img");
+    slapFxElem.classList.add("slap", "slap_anim_"+Math.ceil(Math.random() * 3));
+    slapFxElem.src = "img/site/slap_FX" + Math.ceil(Math.random() * 3) + ".svg";
+    slapFxElem.id = "slap_fx"
+
+    // Add
+    b.insertAdjacentHTML('beforeend', slapFxElem.outerHTML);
+
+    // Get element
+    let nodes = b.querySelectorAll("#slap_fx");
+    let addedEl = nodes[nodes.length-1];
+    addedEl.style.left = pos + "px";
+
+    // Timeout for removal
+    setTimeout(function(){
+        b.removeChild(addedEl);
+    }, 1000)
+}
+
 // Running functions
 textareaSetup();
